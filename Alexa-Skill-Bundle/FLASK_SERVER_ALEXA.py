338a27b8992f9e8d4b95ccbdf512abceb5ebb368
Stebin George <114398468+Stebin-17@users.noreply.github.com>
2023-03-10 01:34:57 +0530

    Update FLASK_SERVER_ALEXA.py

--- Alexa-Skill-Bundle/FLASK_SERVER_ALEXA.py
+++ Alexa-Skill-Bundle/FLASK_SERVER_ALEXA.py
@@ -1,5 +1,3 @@
-import telegram
-from telegram.ext import Updater,MessageHandler
 import logging
 import os
 import paho.mqtt.client as mqtt
@@ -19,10 +17,6 @@ from datetime import datetime
 app = Flask(__name__)
 ask = Ask(app, "/")
 logging.getLogger('flask_ask').setLevel(logging.DEBUG)
-
-STATUSON = ['on','high']
-
-STATUSOFF = ['off','low']
 
 
 @ask.intent("facerecognition")
@@ -96,23 +90,6 @@ def launch():
     speech_text = 'Welcome'
     return question(speech_text).reprompt(speech_text).simple_card(speech_text)
 
-@ask.intent('GpioIntent', mapping = {'status':'status'})
-def Gpio_Intent(status,room):
-    first_client = mqtt.Client()
-    first_client.connect('54.87.92.106',1883)
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)    
-    GPIO.setup(17,GPIO.OUT)
-    if status in STATUSON:
-        GPIO.output(17,GPIO.HIGH)
-        first_client.publish("home/alexa","TURN ON LIGHTS")
-        return statement('turning {} lights'.format(status))
-    elif status in STATUSOFF:
-        GPIO.output(17,GPIO.LOW)
-        first_client.publish("home/alexa","TURN OFF LIGHTS")
-        return statement('turning {} lights'.format(status))
-    else:
-        return statement('Sorry not possible.')
  
 @ask.intent('AMAZON.HelpIntent')
 def help():
