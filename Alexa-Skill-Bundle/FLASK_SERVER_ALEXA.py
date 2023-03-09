432efd85640efddd6fb2255ef685ffbf6b92dcc9
Stebin George <114398468+Stebin-17@users.noreply.github.com>
2023-03-09 15:21:41 +0530

    Update FLASK_SERVER_ALEXA.py

--- Alexa-Skill-Bundle/FLASK_SERVER_ALEXA.py
+++ Alexa-Skill-Bundle/FLASK_SERVER_ALEXA.py
@@ -1,3 +1,5 @@
+import telegram
+from telegram.ext import Updater,MessageHandler
 import logging
 import os
 import paho.mqtt.client as mqtt
@@ -12,59 +14,78 @@ import requests
 import urllib
 from urllib.request import urlopen
 from datetime import datetime
-import threading
 
 
 app = Flask(__name__)
 ask = Ask(app, "/")
 logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
-@ask.intent('chatgpt',mapping={'user_question':'question'})
-def chatgpt(user_question,room):
+STATUSON = ['on','high']
+
+STATUSOFF = ['off','low']
+
+
+@ask.intent("facerecognition")
+def facerecognition():
+    url = 'http://192.168.0.107:1066/static/img/file.json'
+    response = urllib.request.urlopen(url)
+    text = response.read().decode('utf-8')
+    res=json.loads(text)
+    print(res.keys())
+    text1=res['face_name']
+    text2=res['time']
+    timestamp = res['time']
+
+    # Convert the timestamp to datetime object
+    dt_object = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
+
+    # Combine year-month-day and time in hours and minute with am/pm into a single variable
+    text3 = dt_object.strftime('%Y-%m-%d %I:%M %p')
+    return statement("{} is at the door,time is {}".format(text1,text3))
+
+
+
+@ask.intent("dooropen",mapping={'user_question1':'permission'})
+def dooropen(user_question1,room):
     first_client = mqtt.Client()
     first_client.connect('54.87.92.106',1883)
-
-    if session.attributes.get('stop'):
-        session.attributes['stop'] = False
-        return statement("Goodbye")
+    openai.api_key = "sk-*****************************************As0A"
+    chat = user_question1
+    print(chat)
+    if "let him in" in chat.lower() or "he is a good guy" in chat.lower() or "he is my friend" in chat.lower() or "i know him" in chat.lower():
+        first_client.publish("home/door","OPEN THE DOOR")
+        return statement("Opening the doors")
+       
+    elif "he is harmfull" in chat.lower() or "he is bad" in chat.lower() or "he is my enemy" in chat.lower():
+        first_client.publish("home/door","CLOSE THE DOOR")
+        return statement("closing the door")
+        
+    
     else:
-        openai.api_key = "sk-t5Zj7TH***********************ZqyW28aWB2lblsS53N"
-        chat=user_question
-        print(chat)
-        if 'turn on lights' in chat.lower():
-            first_client.publish("home/alexa","TURN ON LIGHTS")
-            return statement("turning on lights")
-
-        elif 'turn  off light' in chat.lower():
-            first_client.publish("home/alexa","TURN OFF LIGHTS")
-            return statement("turning off lights")
-        respond = openai.Completion.create(model="text-davinci-003",
-                                           prompt="Convert \"" + chat + "\" to any one among below commands :\n- Turn off light  \n- Turn on light\n",
-                                           temperature=0,max_tokens=100,
-                                           top_p=1,
-                                           frequency_penalty=0.2,
-                                           presence_penalty=0 )
-        chat1="Convert \"" + chat + "\" to any one among below commands :\n- Turn off light  \n- Turn on light"
-        print(chat1)
-        print(respond)
-        text=respond['choices'][0]['text']
+        response = openai.Completion.create(
+          model="text-davinci-003",
+          prompt="Convert \"" + chat + "\" to any one among below commands :\n- Unlock the door\n- Lock the door \n",
+          temperature=0,
+          max_tokens=100,
+          top_p=1,
+          frequency_penalty=0.2,
+          presence_penalty=0)
+        chat2="Convert \"" + chat + "\" to any one among below commands :\n- Unlock the door\n- Lock the door \n"
+        print(chat2)
+        print(response)
+        text=response['choices'][0]['text']
         print("the response from chatgpt is {}".format(text))
         
-        if "Turn on light" in text or "brightness" in text:
-            first_client.publish("home/alexa","TURN ON LIGHTS")
-            return statement("turning on lights")
-        
-        elif "Turn off light" in text or "dimmer" in text or "Dim" in text or "off" in text:
-            first_client.publish("home/alexa","TURN OFF LIGHTS")
-            return statement("turning off lights")
-        
-        elif "stop" in text or "exit" in text:
-            return statement("Exiting the ChatGPT Skill")
+        if "Unlock the door" in text or "Open" in text:
+            first_client.publish("home/door","OPEN THE DOOR")
+            return statement("Opening the doors")
+            
+        elif "Lock the door" in text or "Close" in text or "Shut" in text or "don't know" in text:
+            first_client.publish("home/door","CLOSE THE DOOR")
+            return statement("closing the door")
+        else:
+            return statement("chatGpt returned {}".format(text))
 
-
-@ask.intent("AMAZON.YesIntent")
-def continue_chatgpt():
-    return chatgpt(None, None)
 
 @ask.intent("AMAZON.NoIntent")
 def stop_chatgpt():
@@ -72,9 +93,26 @@ def stop_chatgpt():
 
 @ask.launch
 def launch():
-    speech_text = 'Welcome to Innovation chat Automation'
+    speech_text = 'Welcome'
     return question(speech_text).reprompt(speech_text).simple_card(speech_text)
 
+@ask.intent('GpioIntent', mapping = {'status':'status'})
+def Gpio_Intent(status,room):
+    first_client = mqtt.Client()
+    first_client.connect('54.87.92.106',1883)
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)    
+    GPIO.setup(17,GPIO.OUT)
+    if status in STATUSON:
+        GPIO.output(17,GPIO.HIGH)
+        first_client.publish("home/alexa","TURN ON LIGHTS")
+        return statement('turning {} lights'.format(status))
+    elif status in STATUSOFF:
+        GPIO.output(17,GPIO.LOW)
+        first_client.publish("home/alexa","TURN OFF LIGHTS")
+        return statement('turning {} lights'.format(status))
+    else:
+        return statement('Sorry not possible.')
  
 @ask.intent('AMAZON.HelpIntent')
 def help():
@@ -90,11 +128,10 @@ def stop():
 def session_ended():
     return "{}", 200
 
-    
-def start_flask_server():
-    app.run(debug=True)
+
 
 if __name__ == '__main__':
+    
     if 'ASK_VERIFY_REQUESTS' in os.environ:
         verify = str(os.environ.get('ASK_VERIFY_REQUESTS', '')).lower()
     
